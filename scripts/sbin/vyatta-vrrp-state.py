c3c9f3f3c1e1a05ee3bb532c3564ed1fb1e384f2
Anthony Dempsey <adempsey@mail.eng.vyatta.net>
2019-01-30 15:22:26 +0000

    Remove interfaces without vrrp-group config
    In the vrrp config there is a default value for start-delay even if
    there's no vrrp config present. Remove these nodes from the config we
    pass around to limit processing.

--- scripts/sbin/vyatta-vrrp-state.py
+++ scripts/sbin/vyatta-vrrp-state.py
@@ -1,7 +1,7 @@
-#! /usr/bin/env python
+#! /usr/bin/python
 
 # **** License ****
-# Copyright (c) 2019 AT&T Intellectual Property.
+# Copyright (c) 2019-2020 AT&T Intellectual Property.
 # All rights reserved.
 # 
 # SPDX-License-Identifier: GPL-2.0-only
