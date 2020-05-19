1bfb1bc338c751bcf78a156c2cd4ac4f588d8bfc
Daniel Gollub <dgollub@att.com>
2020-05-19 12:34:33 +0200

    ipsec-notify: xl2tpd is gone
    VRVDR-45185

--- scripts/sbin/vyatta-ipsec-notify.sh
+++ scripts/sbin/vyatta-ipsec-notify.sh
@@ -17,27 +17,20 @@ TYPE=$1
 NAME=$2
 STATE=$3
 
-# check if there is a active L2TP/IPsec RA VPN server setup
-L2TP_RAVPN=false
-grep -qE '^\[global\]' /etc/xl2tpd/xl2tpd.conf 2> /dev/null && L2TP_RAVPN=true
-
 case $STATE in
         "MASTER")
                   # in case charon was started via stroke earlier. To prevent service file failure.
                   ipsec stop || true
 
                   systemctl restart strongswan | logger -p info -t $(/usr/bin/basename $0)
-                  $L2TP_RAVPN && systemctl restart xl2tpd | logger -p info -t $(/usr/bin/basename $0)
                   exit 0
                   ;;
         "BACKUP")
                   systemctl stop strongswan | logger -p info -t $(/usr/bin/basename $0)
-                  $L2TP_RAVPN && systemctl stop xl2tpd | logger -p info -t $(/usr/bin/basename $0)
                   exit 0
                   ;;
         "FAULT")
                   systemctl stop strongswan | logger -p info -t $(/usr/bin/basename $0)
-                  $L2TP_RAVPN && systemctl stop xl2tpd | logger -p info -t $(/usr/bin/basename $0)
                   exit 0
                   ;;
         *)        echo "unknown state"
