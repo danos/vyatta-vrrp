e078bed27f5116d93342408000ab949b52716567
Daniel Gollub <dgollub@att.com>
2020-11-04 11:13:16 +0100

    ipsec: mitigiate IPsec trigger issue with IPsec VCI rewrite
    VRVDR-53520

--- scripts/sbin/vyatta-ipsec-notify.sh
+++ scripts/sbin/vyatta-ipsec-notify.sh
@@ -23,8 +23,10 @@ case $STATE in
                   # only needed when the script using IPsec stroke is present.
                   if [ -f /opt/vyatta/sbin/vpn-config.pl ]; then
                           ipsec stop || true
+                          run restart vpn | logger -p info -t $(/usr/bin/basename $0)
+                  else
+                          /lib/vci-security-vpn-ipsec/ipsec-op restart-vpn | logger -p info -t $(/usr/bin/basename $0)
                   fi
-                  run restart vpn | logger -p info -t $(/usr/bin/basename $0)
                   exit 0
                   ;;
         "BACKUP")
